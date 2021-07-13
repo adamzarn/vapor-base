5aacc58dde7815b0ac7c2198f6b3fcf778a5b409
Adam Zarn <ajzarn@gmail.com>
2021-07-13 12:10:04 -0500

    Update swift package versions

--- Package.swift
+++ Package.swift
@@ -8,9 +8,9 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.30.0"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc"),
-        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-rc"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.48.1"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.3.1"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.1.3"),
         .package(url: "https://github.com/vapor-community/mailgun.git", from: "5.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "4.1.2"),
     ],
