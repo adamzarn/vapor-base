f2928641d94d9d480431d4f2a42f34ad402ac89e
Adam Zarn <ajzarn@gmail.com>
2021-11-03 01:00:47 -0500

    Add deviceId as an allowed header

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -22,7 +22,7 @@ public func configure(_ app: Application) throws {
     let corsConfiguration = CORSMiddleware.Configuration(
         allowedOrigin: .all,
         allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
-        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin, .accessControlAllowHeaders, .accessControlAllowCredentials]
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin, .accessControlAllowHeaders, .accessControlAllowCredentials, HTTPHeaders.Name("deviceId")]
     )
     let cors = CORSMiddleware(configuration: corsConfiguration)
     app.middleware.use(cors)
